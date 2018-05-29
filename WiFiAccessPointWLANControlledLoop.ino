fa4dc52cf0c04a7d846b2cff39f482655b7aaf77
Michael <bRanger82@users.noreply.github.com>
2018-05-29 06:59:58 +0200

    Update WiFiAccessPointWLANControlledLoop.ino

--- WiFiAccessPointWLANControlledLoop.ino
+++ WiFiAccessPointWLANControlledLoop.ino
@@ -12,75 +12,90 @@ const int GPIO2 = 2;
 
 ESP8266WebServer server(80);
 
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
- * connected to this access point to see it.
- */
+/* Go to http://192.168.4.1 in a web browser connected to this access point to see it. */
 void handleRoot() 
 {
+  /* Build the HTML page which contains the response for requesting the default website */
   String s = "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
   s += "<link rel=\"icon\" href=\"data:,\">";
   s += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}";
   s += ".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;";
   s += "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}";
   s += ".button2 {background-color: #77878A;}</style></head>";
-  s += "<body><h1>ESP8266 Web Server</h1>";
-  s += "<p>GPIO 0 - State</p>";
+  s += "<body><h1>ESP8266 Web Server - Loop Detection Control</h1>";
+  
   if (digitalRead(GPIO0) == HIGH)
   {
-    s += "<p><a href=\"/0/off\"><button class=\"button button2\">OFF</button></a></p>";
+	  s += "<p>GPIO 0 - State is ON</p>";
+	  s += "<p><a href=\"/0/off\"><button class=\"button button2\">OFF</button></a></p>";
   } else
   {
-    s += "<p><a href=\"/0/on\"><button class=\"button\">ON</button></a></p>";
+	  s += "<p>GPIO 0 - State is OFF</p>";
+	  s += "<p><a href=\"/0/on\"><button class=\"button\">ON</button></a></p>";
   }
-  s += "<p>GPIO 2 - State</p>";
+	
   if (digitalRead(GPIO2) == HIGH)
   {
-    s += "<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>";
+	  s += "<p>GPIO 2 - State is ON</p>";
+	  s += "<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>";
   } else
   {
-    s += "<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>";
+	  s += "<p>GPIO 2 - State is OFF</p>";
+	  s += "<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>";
   }
   s += "</body></html>";
-	server.send(200, "text/html", s);
+  server.send(200, "text/html", s);
 }
 
 void redirectPage()
 {
-  String s = "<head>";
-  s += "<meta http-equiv=\"refresh\" content=\"0;url=http://192.168.4.1\">"; 
-  s += "</html>";
-  server.send(200, "text/html", s);server.send(200, "text/html", s);
+	/* 
+	  If this code is returned, the page will automatically be redirected.
+	  This is required because otherwise after each change the webbrowers will
+	  show a timeout message because nothing was received back from this webserver.
+	*/
+	String s = "<head>";
+	s += "<meta http-equiv=\"refresh\" content=\"0;url=http://192.168.4.1\">"; 
+	s += "</html>";
+	server.send(200, "text/html", s);server.send(200, "text/html", s);
+}
+
+void handleGPIO(int PinNo, bool State)
+{
+	if (state)
+	{
+		digitalWrite(PinNo, HIGH);
+	} else
+		digitalWrite(PinNo, LOW);
+	}
+	redirectPage();
 }
 
 void handleGPIO0Off()
 {
-  digitalWrite(0, LOW);
-  redirectPage();
+	handleGPIO(GPIO0, false);
 }
 
 void handleGPIO0On()
 {
-  digitalWrite(0, HIGH);
-  redirectPage();
+	handleGPIO(GPIO0, true);
 }
 
 void handleGPIO2Off()
 {
-  digitalWrite(2, LOW);
-  redirectPage();
+	handleGPIO(GPIO2, false);
 }
 
 void handleGPIO2On()
 {
-  digitalWrite(2, HIGH);
-  redirectPage();
+	handleGPIO(GPIO2, true);
 }
 
 void setup() 
 {
-  pinMode(GPIO0, OUTPUT);
-  pinMode(GPIO2, OUTPUT);
-  digitalWrite(GPIO0, LOW);
+	pinMode(GPIO0, OUTPUT);
+	pinMode(GPIO2, OUTPUT);
+	digitalWrite(GPIO0, LOW);
 	digitalWrite(GPIO2, LOW);
 	delay(1000);
 	Serial.begin(115200);
@@ -93,10 +108,10 @@ void setup()
 	Serial.print("AP IP address: ");
 	Serial.println(myIP);
 	server.on("/", handleRoot);
-  server.on("/0/off", handleGPIO0Off);
-  server.on("/0/on", handleGPIO0On);
-  server.on("/2/off", handleGPIO2Off);
-  server.on("/2/on", handleGPIO2On);  
+	server.on("/0/off", handleGPIO0Off);
+	server.on("/0/on", handleGPIO0On);
+	server.on("/2/off", handleGPIO2Off);
+	server.on("/2/on", handleGPIO2On);  
 	server.begin();
 	Serial.println("HTTP server started");
 }
